df2fc67d19fe80a4996c58e9a2435d7a496855cd
Noy Moshkovitz <noynava1@gmail.com>
2020-01-29 20:48:01 +0200

    WindowChat styled and connected

--- client-project/src/Components/Chat/Chat.jsx
+++ client-project/src/Components/Chat/Chat.jsx
@@ -37,7 +37,7 @@ class ChatWidget extends Component {
     render() {
         return (
             <div className="round hollow text-center">
-                <a href="#" onClick={this.props.onOpen}><i className="far fa-comment"></i> Open in chat </a>
+                <a href="#" onClick={this.props.onOpen}><i className="far fa-comment"></i> Support </a>
             </div>
          );
     }
--- client-project/src/Components/Chat/ChatWindow.jsx
+++ client-project/src/Components/Chat/ChatWindow.jsx
@@ -7,52 +7,76 @@ import socketIOClient from "socket.io-client";
 class ChatWindow extends Component {
     constructor(props) {
         super(props);
-        this.userName = "name";
+        this.userName = props.userName;
         this.state = {
-            endpoint: "localhost:4001",
+            endpoint: "localhost:5000",
             message: '',
             name: '',
             allMessages: []
         };
+        this.updateMessage = this.updateMessage.bind(this);
     }
 
-    getTime() {
-        let date = new Date();
-        return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    componentDidMount = () => {
+        document.body.classList.toggle('chat');
+
+        this.setState({ name: this.userName });
+        const socket = socketIOClient(this.state.endpoint);
+
+        socket.on('message', (message) => {
+            if (message.name !== this.state.name) {
+                let arr = this.state.allMessages;
+                arr.push(message);
+                this.setState({ allMessages: arr });
+            }
+        })
+
+    }
+    componentWillUnmount() {
+        document.body.classList.remove('chat')
+    }
+    updateMessage(e) {
+        this.setState({ message: e.target.value });
     }
 
     render() {
+        const socket = socketIOClient(this.state.endpoint);
         return (
             <div>
-                <span onClick={this.props.onClose}><i className="fas fa-times"></i></span>
                 <div className="container-fluid h-100">
                     <div className="row justify-content-center h-100">
 
                         <div className="col-md-8 col-xl-6 chat">
-                            <div className="card">
-                                <div className="card-header msg_head">
-                                    <div className="d-flex bd-highlight">
-                                        <div className="img_cont">
-                                        <div className="rounded-circle user_img userNameCard">noy</div>
-                                            <span className="online_icon"></span>
+                            <div class="popup-box chat-popup" id="qnimate">
+                                <div className="card">
+                                    
+                                    <div className="card-header msg_head">
+                                        <div className="d-flex bd-highlight">
+                                            <div className="img_cont">
+                                                <div className="rounded-circle user_img userNameCard">{this.userName}</div>
+                                                <span className="online_icon"></span>
+                                            </div>
+                                            <div className="user_info">
+                                                <span>Chat</span>
+                                                <p>{this.state.allMessages.length} Messages </p>
+                                            </div>
+                                            <span class="chat-header-button pull-right" onClick={this.props.onClose}><i className="fas fa-times"></i></span>
                                         </div>
-                                        <div className="user_info">
-                                            <span>Chat</span>
-                                            <p>num </p>
-                                        </div>
+
                                     </div>
-                                </div>
-                                <div className="card-body msg_card_body">
+                                    <div className="card-body msg_card_body">
+            
+                                            {this.renederMessages()}
+            
+                                    </div>
 
-                                    mesegess
-
-                                </div>
-                                <div className="card-footer">
-                                    <div className="input-group">
-                                        <textarea name="" className="roundCorner form-control type_msg " placeholder="Type your message..." value={this.state.message}
-                                            onChange={this.updateMessage}></textarea>
-                                        <div className="input-group-append">
-                                            <span onClick={this.send} className="input-group-text send_btn costumSendBtn"><i className="fas fa-paper-plane"></i></span>
+                                    <div className="card-footer">
+                                        <div className="input-group">
+                                            <textarea name="" className="roundCorner form-control type_msg " placeholder="Type your message..." value={this.state.message}
+                                                onChange={this.updateMessage}></textarea>
+                                            <div className="input-group-append">
+                                                <span onClick={this.send} className="input-group-text send_btn costumSendBtn"><i className="fas fa-paper-plane"></i></span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -61,8 +85,66 @@ class ChatWindow extends Component {
                     </div>
                 </div>
             </div>
+
         );
     }
+
+    getTime() {
+        let date = new Date();
+        return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    }
+
+    send = () => {
+        let newMessage = {
+            type: 'send',
+            content: this.state.message,
+            name: this.state.name,
+            time: this.getTime()
+        }
+        let arr = this.state.allMessages;
+        arr.push(newMessage);
+        this.setState({ allMessages: arr });
+        this.setState({ message: '' });
+
+        let sendMessage = { ...newMessage };
+        sendMessage.type = "recieve";
+        const socket = socketIOClient(this.state.endpoint);
+        socket.emit('message', sendMessage)
+
+    }
+
+    renederMessages() {
+        return this.state.allMessages.map(message => this.getMessage(message));
+    }
+
+    getMessage(message) {
+        if (message.type == 'send') {
+
+            return <div key={message.time} className="d-flex justify-content-end mb-4">
+                <div className="msg_cotainer_send">
+                    {message.content}
+                    <span className="msg_time_send">{message.time}</span>
+                </div>
+                <div className="img_cont_msg">
+                    <div className="rounded-circle user_img_msg littleCircle">{this.state.name}</div>
+                </div>
+            </div>;
+        }
+
+        if (message.type == 'recieve') {
+
+            return <div key={message.time} className="d-flex justify-content-start mb-4">
+                <div className="img_cont_msg">
+                    <div className="rounded-circle user_img_msg littleCircleStart">{message.name}</div>
+                </div>
+                <div className="msg_cotainer">
+                    {message.content}
+                    <span className="msg_time">{message.time}</span>
+                </div>
+            </div>;
+        }
+    }
+
 }
 
 export default ChatWindow;
